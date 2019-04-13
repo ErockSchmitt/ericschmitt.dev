d8f658edb0bd4a42ee6064cbbbfd771b99d99737
Eric Schmitt <ericschmitt181@aol.com>
2019-04-13 16:09:12 -0400

    First Commit of Ericschmitt.dev

---
+++ Template.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Welcome To RougeLite</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include("includes/header_links.html");?>
+</head>
+<body>
+   <?php include("includes/navbar.html");?>
+
+    <div class="container pt-5">
+        <!--EnterContentHere-->    
+
+    </div>
+
+    <?php include("includes/footer.html");?>
+</body>
+<?php include("includes/script_links.html");?>
+</html>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Welcome To RougeLite</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include("includes/header_links.html");?>
+</head>
+<body>
+   <?php include("includes/navbar.html");?>
+
+    <div class="container pt-5">
+        <div class="jumbotron bg-danger text-light">
+            <h1 class="display-4">RougeLite is Currently Under Construction!</h1>
+            <p class="lead">RougeLite Is A Faster Alternitave To The Classic RPG Table Top! <a href="mailto:Contact@rougelite.com">Click Here For More Info</a></p>
+        </div>
+    
+        <div class="col-12 row">
+            <?php  include("includes/display_classes.php"); ?>
+        </div>
+    </div>
+
+    <?php include("includes/footer.html");?>
+</body>
+<?php include("includes/script_links.html");?>
+</html>
