cb93784c5c16d864b273f80e1483852b6b81f257
Eric Schmitt <ericschmitt181@aol.com>
2019-04-13 16:59:31 -0400

    Added new content: icon logo links Sadie's picture email

--- Template.php
+++ Template.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Welcome To RougeLite</title>
+    <title>Eric Schmitt</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php include("includes/header_links.html");?>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Welcome To RougeLite</title>
+    <title>Eric Schmitt</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php include("includes/header_links.html");?>
@@ -11,15 +11,10 @@
 <body>
    <?php include("includes/navbar.html");?>
 
-    <div class="container pt-5">
-        <div class="jumbotron bg-danger text-light">
-            <h1 class="display-4">RougeLite is Currently Under Construction!</h1>
-            <p class="lead">RougeLite Is A Faster Alternitave To The Classic RPG Table Top! <a href="mailto:Contact@rougelite.com">Click Here For More Info</a></p>
-        </div>
-    
-        <div class="col-12 row">
-            <?php  include("includes/display_classes.php"); ?>
-        </div>
+    <div class="container pt-5 ">
+        <p> I am workiong on the site in my free time! Here is a picture of my dog to hold you over!</p>
+        <img class="rounded" width = '100%' src = "images/Sadie.jpg"/> 
+
     </div>
 
     <?php include("includes/footer.html");?>
